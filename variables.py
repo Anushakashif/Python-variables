219b805618874819a47a494c02befc59612d9ff8
Anusha Kashif <160410652+Anushakashif@users.noreply.github.com>
2025-02-27 23:49:06 +0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,55 @@
+print("Hello World")
+
+# Data Types
+
+# String
+name = "Anusha"
+
+# Integer
+age = 20
+
+# Float
+height = 5.8
+
+# Boolean
+is_student = True
+
+# List
+grades = [99, 97, 95, 90, 92]
+
+# Tuple
+coordinates = (3, 5)
+
+# Dictionary
+person = {"name": "Anusha", "age": 20, "height": 5.8}
+
+# Set
+unique_numbers = {1, 2, 3, 4, 5}
+
+print(name, age, height, is_student, grades, coordinates, person, unique_numbers)
+
+a = 5 
+b = 7
+sum = a + b 
+print(sum)
+
+a = 8 - 2
+b = True
+c = 'Anusha'
+d = None
+print(a)
+print(b)
+print(c)
+print(d)
+print('The type of a is', type(a))
+print('The type of a is', type(b))
+print('The type of a is', type(c))
+
+list1 = [8 , 2.3 , [-4, 7] , ['orange', 'apple']]
+print(list1)
+
+tuple1 = (('rabbit', 'sparrow') , ('lion' , 'tiger'))
+print(tuple1)
+
+dict1 = {"name": "Anusha", "age": 20 , 'canvote': True}
+print(dict1)
